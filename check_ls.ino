5ed0125bcc917453dfb01de7910b8107ac5e07e5
khawtx123 <khawthongxiang@gmail.com>
2025-10-28 21:44:55 +0800

    Update check_ls.ino

--- check_ls.ino
+++ check_ls.ino
@@ -1,28 +1,26 @@
-#define DIR_PIN 12
-#define STEP_PIN 13
-#define EN_PIN 14
-#define LS_PIN 27
-#define stepsPerRevolution 3200
-#define delay_time 900
-int threshold = 5; 
+#define EN_PIN 5
+#define DIR_PIN 6
+#define STEP_PIN 7
 
-void setup() {
-  // put your setup code here, to run once:
-}
+#define LS_PIN 1
+#define LED_PIN 4
 
+#define delay_time 20
+#define delay_time_ceil 70
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(DIR_PIN, OUTPUT);
-  pinMode(STEP_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);  
   pinMode(EN_PIN, OUTPUT);
-  pinMode(LS_PIN, INPUT_PULLUP);
+  pinMode(LS_PIN, INPUT); 
   Serial.begin(9600);
   digitalWrite(DIR_PIN, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  bool limit = digitalRead(LS_PIN);                                                                                                                    
+  digitalWrite(EN_PIN, LOW);
+  bool limit = digitalRead(LS_PIN);
   Serial.println(limit);
 }
